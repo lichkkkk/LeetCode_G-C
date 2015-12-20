02bbb1130412436feaacb984c51a9a669110a083
CHANGLI0914 <changli0914@gmail.com>
2015-12-19 23:48:30 -0800

    Add 319

---
+++ 319-BulbSwitcher/solution_chang.cpp
@@ -0,0 +1,13 @@
+/**
+ * The answer in the follwoing link is a perfect explaination.
+ * https://leetcode.com/discuss/75014/math-solution
+ *
+ * Chang Li at UC San Diego
+ * Dec. 19, 2015
+ */
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
