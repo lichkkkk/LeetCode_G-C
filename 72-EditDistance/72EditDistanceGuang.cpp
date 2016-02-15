460599246b5ceff0c5393a099dd56ab4c71116c6
GuangLightman <guanglightman@gmail.com>
2016-02-15 20:54:59 +0800

    Update 72EditDistanceGuang.cpp

--- 72-EditDistance/72EditDistanceGuang.cpp
+++ 72-EditDistance/72EditDistanceGuang.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        
         int len1 = word1.size();
         int len2 = word2.size();
-        
         if(len1 == 0) return len2;
         if(len2 == 0) return len1;
-        
          vector<vector<int>> d(len1+1, vector<int>(len2+1, 0));
          d[0][0] = 0;
-         
          for(int i = 1; i <= len1 ; ++i) 
             d[i][0] = i;
          for(int j = 1; j <= len2 ; ++j) 
             d[0][j] = j;
-         
-         
-         for(int k = 2 ; k <= len1 + len2; ++k){
+         for(int k = 2 ; k <= len1 + len2; ++k)
              for(int i = 1; i < k && i <=len1; ++i){
                  int j = k - i;
                  if(j <= len2){
@@ -28,8 +22,6 @@ public:
                      d[i][j] = min(d1, d2);
                  }
              }
-         }   
-
         return d[len1][len2];
     }
 };
