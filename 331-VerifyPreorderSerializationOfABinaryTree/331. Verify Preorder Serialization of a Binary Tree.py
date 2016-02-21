0d67dde15c1aa7707ad99056ca32681bfac30904
Xiyun <liuxiyun.nku@hotmail.com>
2016-02-20 16:45:30 -0800

    Update 331. Verify Preorder Serialization of a Binary Tree.py

--- 331-VerifyPreorderSerializationOfABinaryTree/331. Verify Preorder Serialization of a Binary Tree.py
+++ 331-VerifyPreorderSerializationOfABinaryTree/331. Verify Preorder Serialization of a Binary Tree.py
@@ -1,30 +1,26 @@
 __author__ = 'liuxiyun'
-# Based on observation, every node should have two child, whether the children is # or number
-# need - there should be "need"number of signs(number or #) in this level
-# n - there are "n"number of number in this level. it means, next level, "need" is 2*n
-# if there are not enough signs in this level, return false
-# if the elememts in the last level are not all #, return false
-# else: return True
-
+# Put each node from preorder into stack
+# If ## in stack, If the previous node is None or #, return False
+# otherwise, remove them and replace the previous node with #
+# if all the nodes have been added once to stack, and stack only has # in it, return True
 class Solution(object):
     def isValidSerialization(self, preorder):
         """
         :type preorder: str
         :rtype: bool
         """
-        if preorder=='':
-            return True
         preorder = preorder.split(",")
-        need = 1
+        stack = []
         i=0
-        while i< len(preorder):
-            if i+need>len(preorder) or need == 0:
-                return False
-            n=0
-            for j in range(need):
-                if preorder[i+j]!='#':
-                    n+=1
-            i+=(need)
-            need = 2*n
-
-        return True if need == 0 else False
+        for i in range(len(preorder)):
+            node = preorder[i]
+            stack.append(node)
+            if node == "#":
+                while len(stack)>1 and stack[-1] == '#' and stack[-2]=="#":
+                    stack.pop()
+                    stack.pop()
+                    if stack == [] or stack[-1]=='#':
+                        return False
+                    else:
+                        stack[-1]="#"
+        return True if stack == ['#'] else False
