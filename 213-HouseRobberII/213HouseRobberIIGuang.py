ebe9785556d2b1952ec5c66bda09c514d0b3c592
GuangLightman <guanglightman@gmail.com>
2016-02-23 10:22:24 +0800

    Update 213HouseRobberIIGuang.py

--- 213-HouseRobberII/213HouseRobberIIGuang.py
+++ 213-HouseRobberII/213HouseRobberIIGuang.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def rob_help(self, nums):
         withSelf = withoutSelf = 0
-        ret = 0
         for n in nums:
             withSelf, withoutSelf = withoutSelf+n, max(withSelf, withoutSelf)
-            ret = max(withSelf, withoutSelf)
-        return ret     
+        return max(withSelf, withoutSelf)    
     def rob(self, nums):
-        return max(nums[0] + self.rob_help(nums[2:-1]), self.rob_help(nums[1:])) if len(nums) else 0 
+        return max(nums[0] + self.rob_help(nums[2:-1]), self.rob_help(nums[1:])) if len(nums) else 0
