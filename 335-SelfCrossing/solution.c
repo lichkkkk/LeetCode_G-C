c0eee3f19dbdfb951cb7144ef1098eba0200fc95
CHANG LI <changli0914@gmail.com>
2016-02-25 15:56:04 -0800

    335. Self Crossing --- Bug Fix

--- 335-SelfCrossing/solution.c
+++ 335-SelfCrossing/solution.c
@@ -13,7 +13,7 @@ bool isSelfCrossing(int* x, int xSize) {
     for (i = 2; i < xSize && x[i] > x[i-2]; i++);
     
     // Judge if the state transfer is legal 
-    if ((++i < xSize) && ((i > 4) || (i==4 && x[i-1]==x[i-3])) && (x[i]+x[i-4] >= x[i-2])) return true;
+    if ((++i < xSize) && ((i > 4 && x[i-1]+x[i-5]>=x[i-3]) || (i==4 && x[i-1]==x[i-3])) && x[i]+x[i-4] >= x[i-2]) return true;
     
     // Shrink from outer space to inner space
     for (; i < xSize && x[i] < x[i-2]; i++);
