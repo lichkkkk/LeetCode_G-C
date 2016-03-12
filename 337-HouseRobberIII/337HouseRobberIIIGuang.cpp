f96d39ac69b979ecb087babafcca0317c014b4a2
Guang.Lightman <sghu@cs.hku.hk>
2016-03-12 15:24:28 +0800

    add 337 can be done in O(n) time. I use lazy O(nlogn) algo.

---
+++ 337-HouseRobberIII/337HouseRobberIIIGuang.cpp
@@ -0,0 +1,36 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    map<TreeNode*, int> Mcan;
+    map<TreeNode*, int> Mcannot;
+    int rob(TreeNode* root) {
+        return try_steal(root, true);
+    }
+    int try_steal(TreeNode* root, bool can){
+        if(root == NULL) return 0;
+        int m = 0, n = 0;
+        if(can){
+            if(Mcan.find(root) != Mcan.end()) 
+                m = Mcan[root]; 
+            else{
+                m = root->val + try_steal(root->left, false) + try_steal(root->right, false);
+                Mcan[root] = m;
+            }
+        }
+        if(Mcannot.find(root) != Mcannot.end()) 
+            n = Mcannot[root]; 
+        else{
+            n = try_steal(root->left, true) + try_steal(root->right, true);
+            Mcannot[root] = n;
+        }
+        return max(m, n);
+    }
+};
