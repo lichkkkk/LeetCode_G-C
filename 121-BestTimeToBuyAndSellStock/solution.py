a825effea47d5b98c10ecbf360b141bdbbf60a15
CHANG LI <changli0914@gmail.com>
2019-10-29 16:04:46 -0400

    add explanation for 121

--- 121-BestTimeToBuyAndSellStock/solution.py
+++ 121-BestTimeToBuyAndSellStock/solution.py
@@ -1,3 +1,11 @@
+"""
+    Key Points:
+        The problem is asking us to find the largest diff between numbers in the list,
+        and requirs the smaller num appears before the larger number. To solve this,
+        we can scan the list from left to right, always memorizing the smallest number
+        we have seen so far. When we see a number larger than current smallest number,
+        we just calculate the diff and update the best profit we have seen.
+"""
 class Solution(object):
     def maxProfit(self, prices):
         """
@@ -9,6 +17,6 @@ class Solution(object):
         prevMinPrice = prices[0]
         currMaxProfit = 0
         for i in range(1, len(prices)):
-            prevMinPrice = min(prevMinPrice, prices[i])
-            currMaxProfit = max(currMaxProfit, prices[i] - prevMinPrice)
+            prevMinPrice = min(prevMinPrice, prices[i]) # update the lowest price we have seen so far
+            currMaxProfit = max(currMaxProfit, prices[i] - prevMinPrice) # update the max possible profit so far
         return currMaxProfit
