dbc61bd98270728835d66570775728857986f670
CHANG LI <changli0914@gmail.com>
2019-10-28 23:26:40 -0400

    add explanation for 202

--- 202-HappyNumber/solution.py
+++ 202-HappyNumber/solution.py
@@ -1,3 +1,12 @@
+"""
+Key Points:
+    1. The approach to determine whether we are looping endlessly, is to
+    check whether the number appeared before in this loop.
+    2. We can use a Set to memorize all number appeared in this loop.
+    3. Given a number, to compute the next replacement of it, we can use
+    a loop, every time we get the mod and divide the number by 10, until
+    it's 0.
+"""
 class Solution(object):
     def isHappy(self, n):
         """
@@ -10,6 +19,6 @@ class Solution(object):
             nextNum = 0
             while n > 0:
                 nextNum += (n % 10) ** 2
-                n = int(n / 10)
+                n = n // 10
             n = nextNum
         return n == 1
