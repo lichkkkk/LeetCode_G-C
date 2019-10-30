0969404b56977319308e398435b342abdbea6c43
CHANG LI <changli0914@gmail.com>
2019-10-30 17:11:07 -0400

    add fix for 121

--- 121-BestTimeToBuyAndSellStock/solution.py
+++ 121-BestTimeToBuyAndSellStock/solution.py
@@ -5,6 +5,12 @@
         we can scan the list from left to right, always memorizing the smallest number
         we have seen so far. When we see a number larger than current smallest number,
         we just calculate the diff and update the best profit we have seen.
+        
+    How to improve:
+        The current implementation is already the most efficient one, which can finish in
+        linear time. However, if the input prices are very large, given the prices
+        should be continuous, we may be able to down sample the input first, and then
+        narrow down the zone to search for the max profit.
 """
 class Solution(object):
     def maxProfit(self, prices):
@@ -16,7 +22,7 @@ class Solution(object):
             return 0
         prevMinPrice = prices[0]
         currMaxProfit = 0
-        for i in range(1, len(prices)):
+        for i in xrange(1, len(prices)):
             prevMinPrice = min(prevMinPrice, prices[i]) # update the lowest price we have seen so far
             currMaxProfit = max(currMaxProfit, prices[i] - prevMinPrice) # update the max possible profit so far
         return currMaxProfit
