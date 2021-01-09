a55e79ec23414eb5de142ce7c6b1b4db6c09230e
CHANG LI <changli0914@gmail.com>
2021-01-09 23:42:35 +0000

    add 121 cc

---
+++ 121-BestTimeToBuyAndSellStock/solution.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+        int max_profit = 0;
+        int min_price = prices[0];
+        for (int i=1; i<prices.size(); i++) {
+            int curr_profit = prices[i] - min_price;
+            max_profit = max(max_profit, curr_profit);
+            min_price = min(min_price, prices[i]);
+        }
+        return max_profit;
+    }
+};
