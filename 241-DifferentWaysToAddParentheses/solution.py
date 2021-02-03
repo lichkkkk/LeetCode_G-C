a058ccb059a0eb687f24b0e7e6b115a297089169
CHANG LI <changli0914@gmail.com>
2021-02-03 20:43:28 +0000

    add 241

---
+++ 241-DifferentWaysToAddParentheses/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        return self.helper(input, {})
+    
+    def helper(self, s, cache):
+        if s in cache:
+            return cache[s]
+        res  = []
+        for i in range(len(s)):
+            if s[i] in ('+', '-', '*'):
+                lres = self.helper(s[:i], cache)
+                rres = self.helper(s[i+1:], cache)
+                res.extend(self.merge(s[i], lres, rres))
+        if len(res) == 0:
+            res.append(int(s))
+        cache[s] = res
+        return res
+    
+    def merge(self, ch, lres, rres):
+        res = []
+        ops = {
+            '+': lambda x, y: x + y,
+            '-': lambda x, y: x - y,
+            '*': lambda x, y: x * y
+        }
+        for l in lres:
+            for r in rres:
+                res.append(ops[ch](l, r))
+        return res
