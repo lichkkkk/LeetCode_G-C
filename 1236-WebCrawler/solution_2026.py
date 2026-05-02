ae264233a4e68c56af39c636109de9f4eee5a857
LICHKKKK <changli0914@gmail.com>
2026-05-02 10:51:04 -0400

    Refactor crawl method to iterative stack-based approach
    Refactor the crawl method to use an iterative approach with a stack for URL crawling instead of recursion.

--- 1236-WebCrawler/solution_2026.py
+++ 1236-WebCrawler/solution_2026.py
@@ -1,19 +1,33 @@
 class Solution:
 
-    def helper(self, startUrl: str, baseUrl: str, urlSet: set[str], visited: set[str], parser: 'HtmlParser') -> None:
-        if startUrl in visited:
-            return
-        visited.add(startUrl)
-        if not startUrl.startswith(baseUrl):
-            return
-        urlSet.add(startUrl)
-        for url in parser.getUrls(startUrl):
-            self.helper(url, baseUrl, urlSet, visited, parser)
+    # def helper(self, startUrl: str, baseUrl: str, urlSet: set[str], visited: set[str], parser: 'HtmlParser') -> None:
+    #     if startUrl in visited:
+    #         return
+    #     visited.add(startUrl)
+    #     if not startUrl.startswith(baseUrl):
+    #         return
+    #     urlSet.add(startUrl)
+    #     for url in parser.getUrls(startUrl):
+    #         self.helper(url, baseUrl, urlSet, visited, parser)
 
+    # def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+    #     urls, visited = set(), set()
+    #     host_name = startUrl.strip().split('/')[2]
+    #     base_url = f'http://{host_name}'
+    #     self.helper(startUrl, base_url, urls, visited, htmlParser)
+    #     return list(urls)
+ 
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        urls, visited = set(), set()
-        host_name = startUrl.strip().split('/')[2]
-        base_url = f'http://{host_name}'
-        self.helper(startUrl, base_url, urls, visited, htmlParser)
-        return list(urls)
+        hostname = startUrl.split('/')[2]
+        base = f'http://{hostname}'
+        stack = [startUrl]
+        visited = {startUrl}
+        while stack:
+            url = stack.pop()
+            for child_url in htmlParser.getUrls(url):
+                if child_url in visited or not child_url.startswith(base):
+                    continue
+                stack.append(child_url)
+                visited.add(child_url)
+        return list(visited)
  
